41180cd5f51fe80be313a2aea411015120e977a8
Giovanni Lauria <giovanni.lauria+coding@gmail.com>
2020-12-30 08:02:33 +0100

    #43 - Commit of 20201230 - 1609311753 - README

--- automatic_readme_generator.sh
+++ automatic_readme_generator.sh
@@ -34,9 +34,9 @@ do
         do
             if [[ $_counter == 0 ]]
             then
-                _temp_output=`echo $_temp_output_raw | sed "s/$i/<a href=\"$_url\/commit\/$i\">$i</a>/g"`
-            else    
-                _temp_output=`echo $_temp_output_raw | sed "s/$i/<br><a href=\"$_url\/commit\/$i\">$i</a>/g"`
+                _temp_output=`echo $_temp_output_raw | sed "s/$i/'<a href=\"$_url\/commit\/$i\">$i<\/a>'/g"`
+            #else    
+                #_temp_output=`echo $_temp_output_raw | sed "s/$i/<br><a href=\"$_url\/commit\/$i\">$i</a>/g"`
             fi
             _temp_output_raw=$_temp_output
             _counter=$(( _counter + 1 ))
