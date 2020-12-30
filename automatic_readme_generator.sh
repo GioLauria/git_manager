41a61ccd0959abe40838b4b998d45fb22c0ecb29
Giovanni Lauria <giovanni.lauria+coding@gmail.com>
2020-12-30 07:42:47 +0100

    #30 - Commit of 20201230 - 1609310567 - README

--- automatic_readme_generator.sh
+++ automatic_readme_generator.sh
@@ -18,7 +18,7 @@ do
     then
         # the first output has to be the REPO INIT and his tag
         #_notes=`echo -e "$_notes <br>-----------------------------<br>"`
-        _notes=`echo -e "<b><a href="$_urltree\${tags[$_index_tags]}">$_notes ${tags[$_index_tags]}</a></b>"`
+        _notes=`echo -e "<b><a href="$_url tree\${tags[$_index_tags]}">$_notes${tags[$_index_tags]}</a></b>"`
         _notes=`echo -e "$_notes <br>-----------------------------"`
         _temp_output_raw=`git log $_first_commit --oneline | grep "Commit of"`
         _notes=`echo -e "$_notes <br>$_temp_output_raw"`      
