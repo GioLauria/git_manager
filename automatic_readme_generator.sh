05564400e669d515877e56349da303a5586b981a
Giovanni Lauria <giovanni.lauria+coding@gmail.com>
2020-12-30 05:52:26 +0100

    #27 - Commit of 20201230 - 1609303945 - Fixing README generator

--- automatic_readme_generator.sh
+++ automatic_readme_generator.sh
@@ -1,4 +1,4 @@
-set -x
+#set -x
 declare -A tags
 _first_commit=`git log --oneline | tail -n1 | awk '{print $1}'|sort -r`
 _list_of_tags=`git tag`
@@ -16,12 +16,12 @@ do
     if [[ $_index_tags == 0 ]]
     then
         # the first output has to be the REPO INIT and his tag
-        _notes=`echo -e "$_notes \`git log $_first_commit --oneline | grep \"Commit of\"\`"`
-        _notes=`echo -e "${tags[$_index_tags]}"`
-      
+        _notes=`echo -e "\n${tags[$_index_tags]}"`
+        _notes=`echo -e "$_notes \n\`git log $_first_commit --oneline | grep \"Commit of\"\`"`      
     else
-        _notes=`echo -e "$_notes \`git log ${tags[$((_index_tags-1))]}..${tags[$((_index_tags))]} --oneline | grep "Commit of"\`"`
         _notes=`echo -e "$_notes \n${tags[$_index_tags]}"`
+        _notes=`echo -e "$_notes \n\`git log ${tags[$((_index_tags-1))]}..${tags[$((_index_tags))]} --oneline | grep "Commit of"\`"`
+        
     fi
     # Show orphan commits (With no tag parent)
     # if [[ $_index_tags == $((_len_array-1)) ]]
@@ -29,7 +29,5 @@ do
     #      _notes=`echo -e "$_notes \n\`git log ${tags[$((_index_tags))]}..HEAD --oneline | grep "Commit of"\`"`
     # fi     
 done
-#_notes=`echo "$_notes" | tac`
-
 sed -i '/RELEASE NOTES/q' README.md
 sed -i '/RELEASE NOTES/r/dev/stdin' README.md <<<"$_notes"
