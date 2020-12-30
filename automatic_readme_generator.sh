a513060ed1e6d43cd3866fe52e992d76a7e692d8
Giovanni Lauria <giovanni.lauria@gmail.com>
2020-12-30 16:16:53 +0100

    Minor changes on generator

--- automatic_readme_generator.sh
+++ automatic_readme_generator.sh
@@ -42,7 +42,7 @@ do
             else    
                 _temp_output=`echo $_temp_output_raw | sed "s;$i;<br><a href=\"$_url/commit/$i\">$i<\/a>;g"`
             fi
-            echo $_temp_output > _temp_output_raw
+            _temp_output_raw=`echo $_temp_output`
             _counter=$(( _counter + 1 ))
             done
         _notes=`echo -e "$_notes <br>$_temp_output"`
